c7c6c9496d8bea3af905c6f7ef6cec39482684bb
Andrew Fields <andybug10@gmail.com>
2016-02-21 13:44:25 -0600

    Add prepare-csv script
    Wrote script to convert Google sheets CSV to
    simple CSV; all invalid records removed

---
+++ src/prepare-csv/prepare-csv.go
@@ -0,0 +1,45 @@
+package main
+
+import "os"
+import "fmt"
+import "encoding/csv"
+
+const usage string =
+	"prepare-csv <filename.csv>\n" +
+    "    prepares file for parsing by gnuplot\n" +
+	"    writes to stdout"
+
+
+/* read all records from csv file */
+func read_csv(filename string) (records [][]string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	reader := csv.NewReader(f)
+	records, read_err := reader.ReadAll()
+	if read_err != nil {
+		panic(read_err)
+	}
+
+	f.Close()
+
+	return records
+}
+
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+
+	records := read_csv(args[0])
+	for _, record := range records[1:] {
+		if len(record[2]) != 0 {
+			fmt.Println(record[0] + "," + record[2])
+		}
+	}
+}
